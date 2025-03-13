1b10659ffe980b91dc4ebac770eed2783d6581f3
nychae <51692255+nychae@users.noreply.github.com>
2025-03-13 14:08:19 +0900

    서버 생성 방식 변경, 웹소켓 서버 생성

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,5 +11,9 @@ app.get("/", (req, res) => res.render("home"))
 app.get("/*", (req, res) => res.redirect("/"))
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+// http서버와 websocket서버 같이 작동하게 함
+// http 서버 위에 websocket 서버를 만들어 같은 포트를 공유함
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
 
+server.listen(3000, handleListen);
