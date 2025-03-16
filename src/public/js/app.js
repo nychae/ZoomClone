fdae3eafbcabeac6ac4362e2ee71fdb0ba20daf2
nychae <51692255+nychae@users.noreply.github.com>
2025-03-16 18:22:42 +0900

    RTCPeerConnection 적용 브라우저간 영상 스트리밍 송수신 완료

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,6 @@
 const socket = io();
 
+const call = document.getElementById('call');
 const myFace = document.getElementById('myFace');
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
@@ -8,6 +9,10 @@ const camerasSelect = document.getElementById('cameras');
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
+
+call.hidden = true;
 
 const getCameras = async() => {
     try {
@@ -53,7 +58,6 @@ const getMedia = async (deviceId) => {
     }
 }
 
-getMedia();
 
 const handleMuteClick = () => {
     myStream.getAudioTracks().forEach((track) => track.enabled = !track.enabled);
@@ -79,10 +83,86 @@ const handleCameraClick = () => {
     }
 }
 
-const handleCameraChange = () => {
-    getMedia(camerasSelect.value);
+const handleCameraChange = async () => {
+    await getMedia(camerasSelect.value);
+    if(myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === 'video');
+        await videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener('click', handleMuteClick);
 cameraBtn.addEventListener('click', handleCameraClick);
 camerasSelect.addEventListener('change', handleCameraChange);
+
+
+// Welcome Form (choose a room)
+
+const welcome = document.getElementById('welcome');
+const welcomeForm = welcome.querySelector('form');
+
+const initCall = async () => {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+const handleWelcomeSubmit = async (event) => {
+    event.preventDefault();
+    const input = welcomeForm.querySelector('input');
+    await initCall();
+    socket.emit('join_room', input.value);
+    roomName = input.value;
+    input.value = '';
+}
+
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
+
+
+// Socket Code
+
+socket.on('welcome_video', async () => {
+    const offer = await myPeerConnection.createOffer();
+    await myPeerConnection.setLocalDescription(offer);
+    console.log('sent the offer');
+    socket.emit('offer', offer, roomName)
+})
+
+socket.on('offer', async (offer) => {
+    console.log('received the offer');
+    await myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    await myPeerConnection.setLocalDescription(answer);
+    console.log('sent the answer');
+    socket.emit('answer', answer, roomName);
+})
+
+socket.on('answer', async (answer) => {
+    console.log('received the answer');
+    await myPeerConnection.setRemoteDescription(answer);
+})
+
+socket.on('ice', async (ice) => {
+    console.log('received candidate');
+    await myPeerConnection.addIceCandidate(ice);
+})
+
+// RTC Code
+const makeConnection = () => {
+    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener('icecandidate', handleIce);
+    myPeerConnection.addEventListener('addstream', handleAddStream);
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+
+}
+
+const handleIce = (data) => {
+    console.log('sent candidate');
+    socket.emit('ice', data.candidate, roomName);
+}
+
+const handleAddStream = (data) => {
+    const peerFace = document.getElementById('peerFace');
+    peerFace.srcObject = data.stream;
+}
--- src/server.js
+++ src/server.js
@@ -84,6 +84,26 @@ wsServer.on('connection', socket => {
 
     /********************************************************************************************/
 
+
+    /*
+    * Video
+    * */
+    socket.on('join_room', (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit('welcome_video');
+    })
+
+    socket.on('offer', (offer, roomName) => {
+        socket.to(roomName).emit('offer', offer);
+    });
+
+    socket.on('answer', (answer, roomName) => {
+        socket.to(roomName).emit('answer', answer);
+    })
+
+    socket.on('ice', (ice, roomName) => {
+        socket.to(roomName).emit('ice', ice);
+    })
 })
 
 
